f5c5fae945303325f65aa093729dec2b511aec16
Danny <slummock@gmail.com>
2016-07-23 21:59:19 +0100

    part 5, using a service to get heroes list async

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     directives: [HeroDetailComponent],
@@ -68,27 +70,23 @@ import { HeroDetailComponent } from './hero-detail.component';
                 margin-right: .8em;
                 border-radius: 4px 0 0 4px;
             }
-            `]
+            `],
+        providers:[HeroService]
 })
 
-export class AppComponent {
-    heroes = HEROES;
+export class AppComponent implements OnInit {
+    constructor(private heroService: HeroService){};
+    heroes = [];
     title = 'Tour of Heroes';
     selectedHero: Hero;
     onSelect(hero: Hero) { this.selectedHero = hero; }
 
-}
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+    ngOnInit() {
+        this.getHeroes();
+    }
+}
 
